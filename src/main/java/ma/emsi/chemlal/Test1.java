54e82f07ab8bd6bb480bfca03396b5475455bba2
Qlife120 <youssech7@gmail.com>
2024-12-15 16:16:11 +0100

    Test 1 passed

---
+++ src/main/java/ma/emsi/chemlal/Test1.java
@@ -0,0 +1,34 @@
+package ma.emsi.chemlal;
+
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.chat.request.ResponseFormat;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+
+import java.time.Duration;
+
+@Slf4j
+public class Test1 {
+
+    
+    private static final String GEMINI_KEY = System.getenv("GEMINI_KEY"); ;
+
+    public static void main(String[] args) {
+
+        
+        ChatLanguageModel model = GoogleAiGeminiChatModel.builder()
+                .apiKey(GEMINI_KEY).modelName("gemini-1.5-flash")
+                .temperature(0.7) // name of the model to use
+                .timeout(Duration.ofSeconds(60))
+                .responseFormat(ResponseFormat.JSON) // format of the response to return to the user (JSON )
+                .build();
+
+        String response = model.generate(("How are you doing?"));
+
+        System.out.println(response);
+
+    }
+}
